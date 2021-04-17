18718dd9cbc57825c254a8aa15a906297bb03fda
setsunaMC <sinnryu810@yahoo.co.jp>
2021-04-18 01:57:00 +0900

    Support configuration url

--- src/modules/github.ts
+++ src/modules/github.ts
@@ -1,6 +1,7 @@
 import { getOctokit } from "@actions/github";
 import { WebhookPayload } from "@actions/github/lib/interfaces";
 import { safeLoad } from "js-yaml";
+import axios from "axios";
 
 const uniq = <T>(arr: T[]): T[] => [...new Set(arr)];
 
@@ -123,6 +124,18 @@ export const GithubRepositoryImpl = {
     configuration: string,
     sha: string
   ): Promise<MappingFile> => {
+    const pattern = /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g;
+    if (pattern.test(configuration)) {
+      const response = await axios.get(configuration);
+      const configObject = safeLoad(response.data);
+
+      if (configObject === undefined) {
+        throw new Error(`failed to load yaml\n${configuration}`);
+      }
+
+      return configObject as MappingFile;
+    }
+
     const githubClient = getOctokit(repoToken);
     const response = await githubClient.repos.getContent({
       owner,
